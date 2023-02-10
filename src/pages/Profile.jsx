7faa6d89f52657458321a133f5de45f817d9c976
Ibrahim Amr <ebrahimamra69@gmail.com>
2023-02-10 23:40:29 +0200

    added the edit functionality to profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,12 @@
+import { updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { auth } from '../Firebase';
+import { toast } from 'react-toastify';
+import { auth, db } from '../Firebase';
 
 const Profile = () => {
+	const [changeName, setChangeName] = useState(false);
 	const [formData, setFormData] = useState({
 		name: auth.currentUser.displayName,
 		email: auth.currentUser.email,
@@ -12,6 +16,33 @@ const Profile = () => {
 	function onLogOut() {
 		auth.signOut();
 		navigate('/');
+	}
+
+	function onChange(e) {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.id]: e.target.value,
+		}));
+	}
+
+	async function onSubmit() {
+		try {
+			if (auth.currentUser.displayName !== name) {
+				// Update name in auth
+				await updateProfile(auth.currentUser, {
+					displayName: name,
+				});
+
+				// Update name in firestore
+				const docRef = doc(db, 'users', auth.currentUser.uid);
+				await updateDoc(docRef, {
+					name,
+				});
+				toast.success('Profile details updated');
+			}
+		} catch (err) {
+			toast.error('Could not update profile details');
+		}
 	}
 	return (
 		<>
@@ -23,8 +54,11 @@ const Profile = () => {
 							type='text'
 							id='name'
 							value={name}
-							disabled
-							className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded mb-6'
+							disabled={!changeName}
+							onChange={onChange}
+							className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded mb-6 ${
+								changeName && 'bg-red-300 focus:bg-red-300 text-white'
+							}`}
 						/>
 						{/* Email Input */}
 						<input
@@ -32,13 +66,18 @@ const Profile = () => {
 							id='email'
 							value={email}
 							disabled
-							className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded mb-6'
+							className={`w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded mb-6 `}
 						/>
 						<div className='flex justify-between items-center whitespace-nowrap text-sm sm:text-lg mb-6'>
 							<p className='flex items-center'>
 								Do you want to change your name?{' '}
-								<span className='text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1 cursor-pointer'>
-									Edit
+								<span
+									onClick={() => {
+										changeName && onSubmit();
+										setChangeName((prevState) => !prevState);
+									}}
+									className='text-red-600 hover:text-red-800 transition ease-in-out duration-200 ml-1 cursor-pointer'>
+									{changeName ? 'Apply Change' : 'Edit'}
 								</span>
 							</p>
 							<p
