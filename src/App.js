bb970d6fef2243312d82c7df24545db1469c7dd2
Ibrahim Amr <ebrahimamra69@gmail.com>
2023-02-10 15:29:32 +0200

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/profile' element={<Profile />} />
+					<Route path='/sign-in' element={<SignIn />} />
+					<Route path='/sign-up' element={<SignUp />} />
+					<Route path='/forgot-password' element={<ForgotPassword />} />
+					<Route path='/Offers' element={<Offers />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
